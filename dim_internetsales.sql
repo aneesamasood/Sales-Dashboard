95ede09a29ea53cf73d0e56db941ffdc42901d43
aneesamasood <146127022+aneesamasood@users.noreply.github.com>
2024-01-29 21:59:33 -0500

    4th commit

--- dim_internetsales.sql
+++ dim_internetsales.sql
@@ -4,27 +4,10 @@ SELECT
     [DueDateKey],
     [ShipDateKey],
     [CustomerKey],
-      --,[PromotionKey]
-      --,[CurrencyKey]
-    --[SalesTerritoryKey],
      [SalesOrderNumber],
-    --[SalesOrderLineNumber]
-    --[RevisionNumber]
     [OrderQuantity],
     [UnitPrice],
-     --[ExtendedAmount]
-     --[UnitPriceDiscountPct]
-     --[DiscountAmount]
-     --[ProductStandardCost]
-     --[TotalProductCost]
     [SalesAmount]
-      --,[TaxAmt]
-     -- ,[Freight]
-     -- ,[CarrierTrackingNumber]
-     -- ,[CustomerPONumber]
-     -- ,[OrderDate]
-     --,[DueDate]
-     -- ,[ShipDate]
   FROM 
     [dbo].[FactInternetSales]
   WHERE
