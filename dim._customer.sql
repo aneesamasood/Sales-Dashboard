fc88ab897d9d4d091b07e464582beef9c6bbece5
aneesamasood <146127022+aneesamasood@users.noreply.github.com>
2024-01-29 21:57:39 -0500

    Add files via upload

---
+++ dim._customer.sql
@@ -0,0 +1,40 @@
+
+--cleaning this
+SELECT 
+  c.customerkey AS CustomerKey, 
+  --,[GeographyKey]
+  --,[CustomerAlternateKey]
+  --,[Title]
+  c.firstname AS [FirstName], 
+  --,[MiddleName]
+  c.lastname AS [LastName], 
+  c.firstname + ' ' + lastname AS [Full Name], 
+  -- combined first and last name
+  -- ,[NameStyle]
+  -- ,[BirthDate]
+  --,[MaritalStatus]
+  -- ,[Suffix]
+  CASE c.gender WHEN 'F' THEN 'Female' WHEN 'M' THEN 'Male' END AS Gender, 
+  --[EmailAddress]
+  --,[YearlyIncome]
+  --,[TotalChildren]
+  --,[NumberChildrenAtHome]
+  --,[EnglishEducation]
+  -- ,[SpanishEducation]
+  --,[FrenchEducation]
+  -- ,[EnglishOccupation]
+  -- ,[SpanishOccupation]
+  --,[FrenchOccupation]
+  --,[HouseOwnerFlag]
+  --,[NumberCarsOwned]
+  -- ,[AddressLine1]
+  -- ,[AddressLine2]
+  --,[Phone]
+  c.datefirstpurchase AS DateFirstPurchase, 
+     --,[CommuteDistance]
+  g.city AS [Customer City] -- join customer city from geography table
+FROM 
+  dbo.dimcustomer AS c 
+  LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey 
+ORDER BY 
+  CustomerKey ASC -- ordered list by customer key
